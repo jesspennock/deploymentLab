ad1d19e15a691e50226dd2623339926dec8bf889
Jess Pennock <jess.pennock@gmail.com>
2022-10-20 13:16:52 -0600

    connecting to rollbar

--- server/server.js
+++ server/server.js
@@ -8,6 +8,16 @@ app.use(express.json())
 app.use(cors())
 app.use(express.static('public'))
 
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '40f294bc36764c8a81cc403c076fc417',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 const {getHTML, getCSS} = require('./controller')
 
 app.get('/', getHTML)
